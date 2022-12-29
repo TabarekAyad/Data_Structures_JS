5e6632ae435c99d6273619250001359e91bb3835
unknown <tabarek.ayad@gmail.com>
2022-12-29 23:32:41 +0300

    added node to the end (append)

--- LinkedList_TA.js
+++ LinkedList_TA.js
@@ -15,9 +15,30 @@ class LinkedList {
         this.head = new Node(data, this.head);
         this.size++;
     }
+
+    //insert last node
+    insertLast(data){
+        let node = new Node(data);
+        let current;
+
+        if(!this.head){
+            this.head = node;
+        }else{
+            current = this.head;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+        }
+
+        this.size++;
+    }
 }
 
 const ll = new LinkedList;
 
 ll.insertFirst(10);
+
+ll.insertLast (20);
+ll.insertFirst(-10);
 console.log(ll);
