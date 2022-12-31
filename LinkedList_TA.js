f11498c146b84702e2ea4822264bbc0d10fe97a5
unknown <tabarek.ayad@gmail.com>
2022-12-31 18:55:33 +0300

    insert at index

--- LinkedList_TA.js
+++ LinkedList_TA.js
@@ -45,7 +45,39 @@ class LinkedList {
         this.size++;
     }
 
-    
+    //insert at index
+    insertAt(data, index){
+        //if index is out of range
+        if(index < 0 || index > this.size){
+            console.log(`The index ${index} is out of range`)
+            return;
+        }
+
+        //if index = 0, the node should be inserted before the head
+        if(index === 0){
+            this.head = new Node(data, this.head);
+            this.size++;
+            return;
+        }
+
+        const node = new Node(data);
+        let previous;
+        let count = 0;
+        let current = this.head;
+
+        while(count < index){
+            previous = current;
+            current = current.next;
+            count++;
+        }
+
+        node.next = current;
+        previous.next = node;
+
+        this.size++;
+
+    }
+
 }
 
 const ll = new LinkedList;
@@ -53,6 +85,9 @@ const ll = new LinkedList;
 ll.insertFirst(10);
 ll.insertLast (20);
 ll.insertFirst(-10);
-
+ll.insertAt(-20,-1);
+ll.insertAt(30,9);
+ll.insertAt(0,1);
+ll.insertAt(5,2);
 ll.printListData();
 //console.log(ll);
